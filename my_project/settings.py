19437c1671970047880927f694922bdfe854b6ae
LihMartins <lihmmartins@gmail.com>
2024-06-06 12:57:47 +0100

    fixing procfile

--- my_project/settings.py
+++ my_project/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-1@%#vbqb8t-c)^7^r7tvm%#h#%lv2y#nepbq0&9vdo$44qf-!&'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['.herokuapp.com']
 
 
 # Application definition
